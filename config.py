86e6a8c4440c04239888f4ebfa56bedee96c8a22
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-03-10 13:37:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ram0069/new-new",
+    "https://github.com/ProGamer097/Oshioopnew",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
